6dd4d5fdb050bf626910d0e19e6d24ba46e28aec
MrCode-2005 <vishnusajeev2005@gmail.com>
2025-11-15 01:30:45 +0530

    add element section for navigation

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// ELEMENT SELECTION
+
+// Select all navigation links + logo
+const navLinks = document.querySelectorAll(".nav-link, .logo");
+
+// Select all individual pages
+const pages = document.querySelectorAll(".page");
+
+// Select burger menu (mobile)
+const burger = document.getElementById("burger");
+const navLinksContainer = document.getElementById("nav-links");
+
